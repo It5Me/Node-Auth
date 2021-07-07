0cea583d502894502d09053901d30272e3613b6e
It5Me <56817602+It5Me@users.noreply.github.com>
2021-07-07 16:24:17 +0700

    jwt

--- app.js
+++ app.js
@@ -26,15 +26,16 @@ mongoose
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
-app.get('/testpostman', (req, res) => {
-  res.send({
-    name: 'Pim',
-  });
-});
-app.get('/set-cookies', (req, res) => {
-  // res.setHeader('Set-Cookies', 'newUser=true');
-  res.cookie('newUser', false);
-  res.send('you got the cookies!');
+
+// app.get('/set-cookies', (req, res) => {
+//   // res.setHeader('Set-Cookies', 'newUser=true');
+
+//   res.send('you got the cookies!');
+// });
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+  // console.log(cookies);
+  res.json(cookies);
 });
 app.use(authRoutes);
 app.listen(3000, () => {
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 const handleError = (err) => {
   console.log(err.message, err.code);
   let errors = { email: '', password: '' };
@@ -15,6 +16,14 @@ const handleError = (err) => {
   }
   return errors;
 };
+
+// 3 days
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, 'Pim piyajiranan secret', {
+    expiresIn: maxAge,
+  });
+};
 module.exports.signup_get = (req, res) => {
   res.render('signup');
 };
@@ -25,7 +34,9 @@ module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
   } catch (error) {
     const errors = handleError(error);
     res.status(400).json(errors);
